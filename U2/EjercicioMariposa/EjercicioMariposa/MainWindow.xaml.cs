60cacdfa1f41341788b41e0043aa14221d0f1c4f
rauulgijon <raulgijon16@gmail.com>
2025-10-21 10:35:06 +0200

    mariposa

---
+++ U2/EjercicioMariposa/EjercicioMariposa/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace EjercicioMariposa
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+          public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
